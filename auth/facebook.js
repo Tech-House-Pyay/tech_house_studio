61db0b4a9039e74ed9d326d1e2f0e32775d7ec72
yethuaungptu <yethuaung481995@gamil.com>
2019-01-06 00:10:53 +0630

    add auth

--- auth/facebook.js
+++ auth/facebook.js
@@ -5,7 +5,7 @@ var User = require('../model/User');
 passport.use(new FacebookStrategy({
     clientID: "300669657250432",
     clientSecret: "6cc118ac742ca9c9dce7a26d50297ec9",
-    callbackURL: "https://techhouse-studio.herokuapp.com/facebook/callback"
+    callbackURL: "https://techhouse-studio.herokuapp.com/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     User.findOrCreate({name: profile.displayName}, {name: profile.displayName,userid: profile.id}, function(err, user) {
