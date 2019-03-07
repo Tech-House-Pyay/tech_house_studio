008ceb02c57d9abe870e8ad5734623e79ddffa82
yethuaungptu <yethuaung481995@gamil.com>
2019-03-07 22:35:42 +0630

    add index page teacher list

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var Course = require('../model/Course');
+var Teacher = require('../model/Teacher');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Course.find({}).populate('teacher_id').exec((err,rtn)=>{
+    if(err) throw err;
+    Teacher.find({},(err2,rtn2)=>{
+      if(err2) throw err2;
+      res.render('index', { title: 'Express',teacher:rtn2, course: rtn });
+    });
+  });
+
 });
 router.get('/tdetail', function(req, res, next) {
   res.render('teacher/t-detail', { title: 'Express' });
